f47087673bdce4f006caf8d75fb0f8c58ef164b2
robertomacedo <macedorss20@gmail.com>
2021-01-20 12:55:15 -0300

    second commit

--- app/models.py
+++ app/models.py
@@ -5,4 +5,5 @@ class Carros(models.Model):
     modelo = models.CharField(max_length=150)
     marca = models.CharField(max_length=20)
     ano = models.IntegerField()
+    data = models.DateTimeField("data"=now)
     
